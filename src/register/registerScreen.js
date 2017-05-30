53c73435b45f76c6ad732cf03e2dc33309461c55
S K <xz64@users.noreply.github.com>
2017-05-30 01:21:18 +0000

    feat: redirect user to home after registration

--- src/register/registerScreen.js
+++ src/register/registerScreen.js
@@ -1,19 +1,16 @@
 import { defineElement as el, defineView as vw } from 'domvm';
 
-import { localizedText as lt } from '../helpers/localizedText';
+import router from '../router';
 import registerForm from './registerForm';
 
 export default (vm) => {
-  let successfulRegister = false;
-
   vm.on({
     success: () => {
-      successfulRegister = true;
-      vm.redraw();
+      router.path('/');
     },
   });
 
   return () => el('.w-100.w-60-m.w-30-l.center', [
-    successfulRegister ? lt('registration-successful') : vw(registerForm),
+    vw(registerForm),
   ]);
 };
