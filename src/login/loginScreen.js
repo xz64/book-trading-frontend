431825f5c00bdd819dfec2be889f05cbaf2b5f68
S K <xz64@users.noreply.github.com>
2017-06-11 18:11:07 +0000

    feat: add link to register on login page

--- src/login/loginScreen.js
+++ src/login/loginScreen.js
@@ -1,7 +1,9 @@
 import { defineElement as el, defineView as vw } from 'domvm';
 
+import { localizedText as lt } from '../helpers/localizedText';
 import loginForm from './loginForm';
 
 export default () => () => el('.w-100.w-60-m.w-30-l.center', [
   vw(loginForm),
+  el('a', { href: '/#!/register' }, [lt('register')]),
 ]);
